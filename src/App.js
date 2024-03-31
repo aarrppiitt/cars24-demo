9a7ed2c1356e1dc9203dd6b497ba69ec69a87cc5
Arpit Agarwal <arpit@upswing.one>
2024-03-31 08:38:25 +0530

    adding event to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  window.dataLayer.push({
+    event: "pageview",
+  });
   return (
     <div className="App">
       <header className="App-header">
